4aadccbdb4881915430182e147a8d494eeee50d8
duanhx <duanhx@tsinghua.edu.cn>
2013-04-25 17:45:04 +0800

    external

---
+++ external.h
@@ -0,0 +1,11 @@
+extern Configuration config;
+extern ResolverList resolvers;
+extern Policy policy;
+extern QueryList queries;
+
+/*
+extern pthread_t tid_listener;
+extern pthread_t *tid_dispatchers;
+extern pthread_t tid_sender;
+*/
+
